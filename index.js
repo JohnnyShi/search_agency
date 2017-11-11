b1268d898ebc1e8aa14d18a9ab184f0736f92950
JohnnyShi <shiyuan1994@gmail.com>
2017-11-11 16:58:35 -0500

    version 0.1
    add html and css

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// // routing
+// routing
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.sendFile(path.join(__dirname, 'main.html'));
 });
 
 app.listen(app.get('port'), function() {
